f288af0a0a7dbcd374a3d4ed1f8c684c52849796
DoomyTheFroomy <yapdoomman@web.de>
2014-04-02 09:56:48 +0200

    fixed broken scraper

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
-'''
-  Extracting machine readable address data of German "Ausländerbehörden".
-  Version, 4 June 2013.
-  @author johnjohndoe 
-'''
+#  Extracting machine readable address data of German "Ausländerbehörden".
+# Version, 4 June 2013.
+#  @author johnjohndoe 
+
 
 import scraperwiki
 import lxml.html
